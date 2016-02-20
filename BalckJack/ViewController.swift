465ae31b97f32d4cf6a148cb3468c092a94fa0ff
lk23286 <lk23286@gmail.com>
2016-02-20 21:12:50 +0100

    started code

--- BalckJack/ViewController.swift
+++ BalckJack/ViewController.swift
@@ -13,6 +13,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //Blackjack
+        
+        
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
